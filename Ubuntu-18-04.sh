d6e91fbd6dbf9303c2b489b06e8210873ba77e41
icodefortacos <55783482+icodefortacos@users.noreply.github.com>
2020-03-25 04:06:48 -0500

    Minor changes, added instructions on bash script.

--- Ubuntu-18-04.sh
+++ Ubuntu-18-04.sh
@@ -1,4 +1,5 @@
-#Ubuntu 18.04 TLS
+#Ubuntu 18.04 TLS | Docker Compose
+#To run execute bash Ubuntu-18-04.sh
 
 git clone git clone https://github.com/crowdsourcemedical/volunteer-database
 curl -fsSL https://get.docker.com -o get-docker.sh
